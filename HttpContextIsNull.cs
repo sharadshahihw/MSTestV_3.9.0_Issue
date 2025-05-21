57590c71212c154b4079efe6071a5b02b40fdc2a
Sharad Shahi <sshahi@healthwise.org>
2025-05-21 13:58:11 -0500

    added files

---
+++ HttpContextIsNull.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Web.SessionState;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeirdIssueInMSTest3._9._0
+{
+    /*
+     * Issue: HttpContext is null in [TestMethod] after setting it up in [Test Initialize]
+     *
+     *
+     */
+    [TestClass]
+    public class HttpContextIsNull
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            HttpContext.Current = FakeHttpContext();
+        }
+
+        [TestMethod]
+        public void HttpContext_IsNotNull()
+        {
+            Assert.IsNotNull(HttpContext.Current);
+        }
+
+        public static HttpContext FakeHttpContext()
+        {
+            var httpRequest = new HttpRequest("", "http://localhost:7000/", "");
+            var stringWriter = new StringWriter();
+            var httpResponse = new HttpResponse(stringWriter);
+            var httpContext = new HttpContext(httpRequest, httpResponse);
+
+            var sessionContainer = new HttpSessionStateContainer("id", new SessionStateItemCollection(),
+                new HttpStaticObjectsCollection(), 10, true,
+                HttpCookieMode.AutoDetect,
+                SessionStateMode.InProc, false);
+
+            httpContext.Items["AspSession"] = typeof(HttpSessionState).GetConstructor(
+                    BindingFlags.NonPublic | BindingFlags.Instance,
+                    null, CallingConventions.Standard,
+                    new[] { typeof(HttpSessionStateContainer) },
+                    null)
+                .Invoke(new object[] { sessionContainer });
+
+            return httpContext;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WeirdIssueInMSTest3.9.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Healthwise, Inc.")]
+[assembly: AssemblyProduct("WeirdIssueInMSTest3.9.0")]
+[assembly: AssemblyCopyright("Copyright © Healthwise, Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cd05a17-b6c1-482a-805d-7edd5a41f259")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
